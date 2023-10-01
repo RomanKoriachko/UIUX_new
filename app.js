5440c9d2ba2026852bbec005a4bc44bb928f510f
Roman <mazaxaka.tyt@gmail.com>
2023-10-01 14:17:55 +0300

    add adaptive in review section

--- app.js
+++ app.js
@@ -50,6 +50,27 @@ $(document).ready(function () {
         slidesToShow: 4,
         slidesToScroll: 1,
         dots: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    adaptiveHeight: true,
+                },
+            },
+        ],
     });
 });
 
